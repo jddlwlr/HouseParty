2c3a39e28eca6a3be9b93464c17c8d0385058210
Doonywoony <javismcarr@gmail.com>
2022-04-09 10:10:43 -0400

    added a dashboard styling

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,23 +1,31 @@
 import React from "react";
-import username from "";
+//import username from "";
 
 function User() {
   return (
     <section>
-      <div className="container">
-        <h3 className="#username">{username}</h3>
+      <div className="container" id="user">
+        <h3 className="#username">username</h3>
 
         <h2 className="#updates">Upcoming/Live Parties</h2>
         <div>
-          <a href="#invites">My Invites</a>
-
-          <a href="#myParties">My Parties</a>
-
-          <a href="#recentParties">Recent Parties</a>
-
-          <a href="#createParty">Create New Party!</a>
-
-          <a href="#friends">My Friends</a>
+          <ul class="action-list">
+            <li class="item">
+              <a href="#invites">My Invites</a>
+            </li>
+            <li class="item">
+              <a href="#myParties">My Parties</a>
+            </li>
+            <li class="item">
+              <a href="#recentParties">Recent Parties</a>
+            </li>
+            <li class="item">
+              <a href="#createParty">Create New Party!</a>
+            </li>
+            <li class="item">
+              <a href="#friends">My Friends</a>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
