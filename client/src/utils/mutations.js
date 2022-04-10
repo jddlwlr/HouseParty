0aaec35dcee50673732620a0893efe4ceb09c878
Jesse Lawler <jedd.lawler@gmail.com>
2022-04-10 09:22:15 -0600

    buggy

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -31,37 +31,3 @@ export const LOGIN = gql`
     }
   }
 `;
-
-export const RULE = gql`
-  mutation addRule($name: String!, $party: Party) {
-    addRule(name: $name, Party: $party) {
-      _id
-      name
-      party
-    }
-  }
-`;
-
-export const ADD_PARTY = gql`
-  mutation addParty(
-    $name: String!
-    $users: [User]
-    $rules: [Rule]
-    $startDate: String!
-    $endDate: String!
-  ) {
-    addParty(
-      name: $name
-      user: $users
-      rule: $rules
-      startdate: $startDate
-      endDate: $endDate
-    ) {
-      name
-      users
-      rules
-      startDate
-      endDate
-    }
-  }
-`;
