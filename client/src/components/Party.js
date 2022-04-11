34dd3a670b4d5ad4ce7fc1eb89d03c91a97d2967
Doonywoony <javismcarr@gmail.com>
2022-04-10 18:49:31 -0600

    added Link and auth to party.js

--- client/src/components/Party.js
+++ client/src/components/Party.js
@@ -1,34 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { rules } from "../rules";
+import Auth from "../utils/auth";
 //import partyName from "";
 
 function Party() {
   return (
     <section>
-      <div className="container" id="">
-        <div>
-          <h2>House Party</h2>
-          <p>
-            {" "}
-            If any of these events below happen, you will be alerted to take a
-            drink!
-          </p>
-        </div>
+      {Auth.loggedIn() ? (
+        <>
+          <div className="container" id="">
+            <div>
+              <h2>House Party</h2>
+              <p>
+                {" "}
+                If any of these events below happen, you will be alerted to take
+                a drink!
+              </p>
+            </div>
 
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              {rules.map((rule) => (
-                <div className="rules contents row column ">
-                  <button className="triggerBtn" onClick={alert}>
-                    <h1 className="ruleCard">{rule.trigger}</h1>
-                  </button>
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  {rules.map((rule) => (
+                    <div className="rules contents row column ">
+                      <button className="triggerBtn" onClick={alert}>
+                        <h1 className="ruleCard">{rule.trigger}</h1>
+                      </button>
+                    </div>
+                  ))}
                 </div>
-              ))}
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <p>
+          {" "}
+          You need to be logged in play! Please <Link to="/signin">
+            login
+          </Link>{" "}
+          or <Link to="/signUp">signup.</Link>
+        </p>
+      )}
     </section>
   );
 }
